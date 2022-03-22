c32664f198c2b6ae334d0729f9f393305b7afb47
Isaac yanez <isaac231467@gmail.com>
2022-03-21 19:03:32 -0500

    test

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+// * importing apolloserver
+const { ApolloServer } = require('apollo-server-express');
+
+// * import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const startServer = async () => {
+  // * create a new apollo server and pass in our schema data
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    // context: authMiddleware
+  });
+
+  // * start the apollo server
+  await server.start();
+
+  // * integrate our Apollo server with the Express application as middleware
+  server.applyMiddleware({ app });
+
+  // * log where we can to test our GQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath} `);
+};
+
+startServer();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
